d35b4a1f5e4b17d3c16709c6d334a8f18b1fda18
Brandon Risberg <brandon.risberg@gmail.com>
2019-10-20 01:42:07 -0700

    Updates

---
+++ precache-manifest.a27242f244bafbe73dacc7d5e0046695.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "002303c8fce4155a3a8ed0573c855dcd",
+    "url": "/unfolding-game/index.html"
+  },
+  {
+    "revision": "b351f059086d0996b0e1",
+    "url": "/unfolding-game/static/css/main.dfca195d.chunk.css"
+  },
+  {
+    "revision": "574a5bba1c1d0c6811be",
+    "url": "/unfolding-game/static/js/2.ec222f52.chunk.js"
+  },
+  {
+    "revision": "b351f059086d0996b0e1",
+    "url": "/unfolding-game/static/js/main.a403f88d.chunk.js"
+  },
+  {
+    "revision": "43e981bdb307618a9d3a",
+    "url": "/unfolding-game/static/js/runtime-main.d216cd8c.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/unfolding-game/static/media/logo.25bf045c.svg"
+  }
+]);
