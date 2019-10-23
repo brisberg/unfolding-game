7cdcdf3e98ad9f86220ff3865b50769053fc9275
brisberg <brandon.risberg@gmail.com>
2019-10-23 00:51:46 -0700

    Title and Log of App at the top

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -8,17 +8,12 @@ const App: React.FC = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          You find yourself in a dark room...
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        <button>Click Me!</button>
+      </div>
     </div>
   );
 }
