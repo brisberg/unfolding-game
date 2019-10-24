83d140285f09ad6c2e93ebd5d72c6858077044ec
brisberg <brandon.risberg@gmail.com>
2019-10-23 18:06:46 -0700

    Tracking two bits of state with useState with two action buttons

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
-  let counter1 = 0;
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
 
   return (
     <div className="App">
@@ -14,9 +15,16 @@ const App: React.FC = () => {
         </p>
       </header>
       <div>
-        <p>You have clicked {counter1} times.</p>
-        <button>Click Me!</button>
+        <p>You have performed action one {count1} times.</p>
+        <button onClick={() => setCount1(count1 + 1)}>Action One</button>
       </div>
+      {count1 >= 5 &&
+        <div>
+          <p>A new event has occured!</p>
+          <p>You have performed action two {count2} times.</p>
+          <button onClick={() => setCount2(count2 + 1)}>Action Two</button>
+        </div>
+      }
     </div>
   );
 }
